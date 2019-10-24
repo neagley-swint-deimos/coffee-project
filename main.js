ad30fce0a3f6617dbe9ffbd1a19598ddb55a586b
andrewswint <andrewswint1976@gmail.com>
2019-10-24 16:56:35 -0500

    done for the day

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //-------------------calls the coffee to the screen-------------------
 function renderCoffee(coffee) {
     var html = '<div class="coffee">';
-    html += '<div class="genDiv">' + "<h1>" + coffee.name + "&emsp;" + "</h1>" + coffee.roast +  '</div>';
+    html += '<div class="genDiv">' + "<h3>" + coffee.name + "&emsp;" + "</h3>" + coffee.roast +  '</div>';
     html += '</div>';
 
     return html;
